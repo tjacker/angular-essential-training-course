c3b616e6071070d4695bbd89af175a2d8aa6cbf2
Tim Acker <tjacker31@gmail.com>
2021-08-29 13:32:39 -0400

    Getting data to the component with @Input

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
-  styleUrls: ['./media-item.component.css']
+  styleUrls: ['./media-item.component.css'],
 })
 export class MediaItemComponent {
+  @Input() mediaItem;
+
   onDelete() {
     console.log('deleted');
   }
