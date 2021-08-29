b7cc47a3b87d5b2090352bcfe554eb384ebe86a5
Tim Acker <tjacker31@gmail.com>
2021-08-29 14:09:24 -0400

    Using directive values

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,8 +1,11 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
-  selector: '[mwFavorite]'
+  selector: '[mwFavorite]',
 })
 export class FavoriteDirective {
+  @Input() set mwFavorite(value) {
+    this.isFavorite = value;
+  }
   @HostBinding('class.is-favorite') isFavorite = true;
 }
