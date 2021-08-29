08a8a0a05a9f2c45d3781b370c27d5b8d09df24b
Tim Acker <tjacker31@gmail.com>
2021-08-29 18:27:13 -0400

    Template-driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
@@ -8,19 +9,15 @@ import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
-    MediaItemFormComponent
+    MediaItemFormComponent,
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
-  styleUrls: ['./media-item-form.component.css']
+  styleUrls: ['./media-item-form.component.css'],
 })
-export class MediaItemFormComponent {}
+export class MediaItemFormComponent {
+  onSubmit(mediaItemForm) {
+    console.log(mediaItemForm.value);
+  }
+}
