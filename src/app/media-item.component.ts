a79e193d45c889fef9330165502c53b12b56d09d
Tim Acker <tjacker31@gmail.com>
2021-08-29 13:18:02 -0400

    Interpolation and the expression context

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
-  styleUrls: ['./media-item.component.css']
+  styleUrls: ['./media-item.component.css'],
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  name = 'The Redemption';
+
+  wasWatched() {
+    return true;
+  }
+}
