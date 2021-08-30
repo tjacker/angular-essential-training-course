7ef15b1fa6476ce67233f05cb869f65ec698f6e8
Tim Acker <tjacker31@gmail.com>
2021-08-30 19:59:43 -0400

    The Angular HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
@@ -10,23 +11,16 @@ import { MediaItemFormComponent } from './media-item-form.component';
 import { lookupListToken, lookupLists } from './providers';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
-    MediaItemFormComponent
+    MediaItemFormComponent,
   ],
-  providers: [
-    { provide: lookupListToken, useValue: lookupLists }
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  providers: [{ provide: lookupListToken, useValue: lookupLists }],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
