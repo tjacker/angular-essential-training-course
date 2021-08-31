674d9e45836717ed770ac5d30aaf0c4b0be3264d
Tim Acker <tjacker31@gmail.com>
2021-08-30 20:31:17 -0400

    Use HttpClient for POST, PUT, and DELETE calls

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { MediaItemService } from './media-item.service';
 import { lookupListToken } from './providers';
 
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
-  styleUrls: ['./media-item-form.component.css']
+  styleUrls: ['./media-item-form.component.css'],
 })
 export class MediaItemFormComponent implements OnInit {
   form: FormGroup;
@@ -14,15 +19,19 @@ export class MediaItemFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private mediaItemService: MediaItemService,
-    @Inject(lookupListToken) public lookupLists) {}
+    @Inject(lookupListToken) public lookupLists
+  ) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
       medium: this.formBuilder.control('Movies'),
-      name: this.formBuilder.control('', Validators.compose([
-        Validators.required,
-        Validators.pattern('[\\w\\-\\s\\/]+')
-      ])),
+      name: this.formBuilder.control(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('[\\w\\-\\s\\/]+'),
+        ])
+      ),
       category: this.formBuilder.control(''),
       year: this.formBuilder.control('', this.yearValidator),
     });
@@ -41,13 +50,13 @@ export class MediaItemFormComponent implements OnInit {
       return {
         year: {
           min: minYear,
-          max: maxYear
-        }
+          max: maxYear,
+        },
       };
     }
   }
 
   onSubmit(mediaItem) {
-    this.mediaItemService.add(mediaItem);
+    this.mediaItemService.add(mediaItem).subscribe();
   }
 }
--- src/app/media-item-list.component.ts
+++ src/app/media-item-list.component.ts
@@ -4,7 +4,7 @@ import { MediaItemService, MediaItem } from './media-item.service';
 @Component({
   selector: 'mw-media-item-list',
   templateUrl: './media-item-list.component.html',
-  styleUrls: ['./media-item-list.component.css']
+  styleUrls: ['./media-item-list.component.css'],
 })
 export class MediaItemListComponent implements OnInit {
   medium = '';
@@ -17,14 +17,15 @@ export class MediaItemListComponent implements OnInit {
   }
 
   onMediaItemDelete(mediaItem) {
-    this.mediaItemService.delete(mediaItem);
+    this.mediaItemService
+      .delete(mediaItem)
+      .subscribe(() => this.getMediaItems(this.medium));
   }
 
   getMediaItems(medium) {
     this.medium = medium;
-    this.mediaItemService.get(medium)
-      .subscribe(mediaItems => {
-        this.mediaItems = mediaItems;
-      });
+    this.mediaItemService.get(medium).subscribe((mediaItems) => {
+      this.mediaItems = mediaItems;
+    });
   }
 }
