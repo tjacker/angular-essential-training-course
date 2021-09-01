a0b6074ab77aa71b08e347aef7a4bc0281258df0
Tim Acker <tjacker31@gmail.com>
2021-09-01 17:06:58 -0400

    Using common CSS selectors

--- src/app/category-list.component.ts
+++ src/app/category-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: "mw-category-list",
+  selector: 'mw-category-list',
   template: `
     <span class="label" *ngFor="let category of categories">
       {{ category }}
     </span>
   `,
   styles: [
-    ``
-  ]
+    `
+      span {
+        display: inline-block;
+        margin-right: 4px;
+        margin-bottom: 4px;
+      }
+      .label {
+        background-color: #999;
+        color: #fff;
+        border-radius: 4px;
+        padding: 2px 6px;
+        font-style: italic;
+        width: max-content;
+      }
+    `,
+  ],
 })
 export class CategoryListComponent {
   @Input() categories: string[];
